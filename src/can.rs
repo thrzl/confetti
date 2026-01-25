386e21ff71567b84d3703d5c18ffd31e9bed53db
te <thrzl@icloud.com>
2026-01-25 16:53:10 -0500

    wip(can): huge CAN refactor

--- src/can.rs
+++ src/can.rs
@@ -1,6 +1,3 @@
-use std::ops::Add;
-use std::time::{Duration, SystemTime};
-
 use thiserror::Error;
 pub use wpihal::{can as hal_can, can_api};
 
@@ -15,34 +12,142 @@ pub enum HALError {
 
 type HALResult<T> = Result<T, HALError>;
 
+#[repr(u32)]
+pub enum ApiClass {
+    Setpoint = 0,
+    Control = 1,
+    Status = 46,
+}
+
 // see https://github.com/grayson-arendt/sparkcan/blob/25167e908c9350a0047edc041e0a6420b6b77a76/include/SparkBase.hpp#L54C1-L78C3
 #[derive(Clone, Copy)]
-pub enum CANCommand {
-    /// for when we don't care what the value is
-    Null = 0,
+pub enum SparkCANFrame {
+    Null,
 
-    ClearFaults = (6 << 4) | 14,
-    FactoryDefaults = (7 << 4) | 4,
-    FactoryReset = (7 << 4) | 5,
-    Identify = (7 << 4) | 6,
-    Heartbeat = (11 << 4) | 2,
-    BurnFlash = (63 << 4) | 2,
-    FirmwareVersion = (9 << 4) | 8,
+    Heartbeat,
 
-    Setpoint = (0 << 4) | 1,
-    DutyCycle = (0 << 4) | 2,
-    Velocity = (1 << 4) | 2,
-    SmartVelocity = (1 << 4) | 3,
-    Position = (3 << 4) | 2,
-    Voltage = (4 << 4) | 2,
-    Current = (4 << 4) | 3,
-    SmartMotion = (5 << 4) | 2,
+    DutyCycle {
+        setpoint: f32,
+        arb_feedforward: i16,
+        pid_slot: u8,
+        ff_units: FeedforwardUnits,
+    },
+    Velocity {
+        setpoint: f32,
+        arb_feedforward: i16,
+        pid_slot: u8,
+        ff_units: FeedforwardUnits,
+    },
+    Position {
+        setpoint: f32,
+        arb_feedforward: i16,
+        pid_slot: u8,
+        ff_units: FeedforwardUnits,
+    },
 
-    Period0 = (6 << 4) | 0,
-    Period1 = (6 << 4) | 1,
-    Period2 = (6 << 4) | 2,
-    Period3 = (6 << 4) | 3,
-    Period4 = (6 << 4) | 4,
+    Voltage {
+        setpoint: f32,
+        arb_feedforward: i16,
+        pid_slot: u8,
+        ff_units: FeedforwardUnits,
+    },
+    Current {
+        setpoint: f32,
+        arb_feedforward: i16,
+        pid_slot: u8,
+        ff_units: FeedforwardUnits,
+    },
+
+    // statuses
+    Status0 {
+        applied_output: f32,
+        voltage: f32,
+        current: f32,
+        motor_temperature: u8,
+        hard_forward_limit_reached: bool,
+        hard_reverse_limit_reached: bool,
+        soft_forward_limit_reached: bool,
+        soft_reverse_limit_reached: bool,
+        is_inverted: bool,
+    },
+    Status2 {
+        velocity: f32,
+        position: f32,
+    },
+}
+
+impl SparkCANFrame {
+    pub fn arb_id(&self, device_id: u32) -> u32 {
+        let frame_arb_id = match self {
+            Self::Velocity { .. } => 0x0200_000,
+            Self::DutyCycle { .. } => 0x0200_080,
+            Self::Position { .. } => 0x0200_100,
+            Self::Voltage { .. } => 0x0200_140,
+            Self::Current { .. } => 0x0200_180,
+            Self::Heartbeat => 0xB2,
+            _ => unimplemented!("we will never need to get the arb ID of a status"),
+        };
+        frame_arb_id | (device_id << 6)
+    }
+    pub fn to_can_bytes(&self) -> [u8; 8] {
+        let (sp, ff, slot, units) = match *self {
+            Self::Velocity {
+                setpoint,
+                arb_feedforward,
+                pid_slot,
+                ff_units,
+            }
+            | Self::DutyCycle {
+                setpoint,
+                arb_feedforward,
+                pid_slot,
+                ff_units,
+            }
+            | Self::Position {
+                setpoint,
+                arb_feedforward,
+                pid_slot,
+                ff_units,
+            }
+            | Self::Voltage {
+                setpoint,
+                arb_feedforward,
+                pid_slot,
+                ff_units,
+            }
+            | Self::Current {
+                setpoint,
+                arb_feedforward,
+                pid_slot,
+                ff_units,
+            } => (setpoint, arb_feedforward, pid_slot & 0x03, ff_units),
+            _ => unimplemented!("we will never need to convert statuses to CAN bytes"),
+        };
+
+        let mut data = [0u8; 8];
+
+        data[0..4].copy_from_slice(&sp.to_le_bytes());
+        data[4..6].copy_from_slice(&ff.to_le_bytes());
+        data[6] = slot | ((units as u8) << 2);
+        data
+    }
+
+    pub fn from_can_bytes(arb_id: u32, bytes: [u8; 8]) -> Self {
+        let api_class = (arb_id >> 10) & 0x3F;
+        let api_index = (arb_id >> 0) & 0x3F;
+
+        todo!()
+    }
+
+    pub fn heartbeat(device_id: u32) -> u32 {
+        Self::Heartbeat.arb_id(device_id)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum FeedforwardUnits {
+    Voltage = 0,
+    DutyCycle = 1,
 }
 
 pub enum IdleMode {
@@ -50,115 +155,17 @@ pub enum IdleMode {
     Coast,
 }
 
-pub enum CANResponse {
-    Period0(Period0Status),
-    Period1(Period1Status),
-    Period2(Period2Status),
-    Period3(Period3Status),
-    Period4(Period4Status),
-}
-
-pub struct Period0Status {
-    pub timestamp: SystemTime,
-
-    pub duty_cycle: f32,
-    pub faults: u64,
-    pub sticky_faults: u64,
-    pub is_inverted: bool,
-    pub idle_mode: IdleMode,
-    pub is_follower: bool,
-}
-pub struct Period1Status {
-    pub timestamp: SystemTime,
-
-    pub temperature: f32,
-    pub velocity: f32,
-    pub voltage: f32,
-    pub current: f32,
-}
-pub struct Period2Status {
-    pub timestamp: SystemTime,
-
-    pub iAccum: f32,
-    pub position: f32,
-}
-pub struct Period3Status {
-    pub timestamp: SystemTime,
-
-    pub analog_voltage: f32,
-    pub analog_velocity: f32,
-    pub analog_position: f32,
-}
-pub struct Period4Status {
-    pub timestamp: SystemTime,
-}
-
-pub trait CANResponseStatus {
-    fn decode(raw_value: u64, timestamp: u32) -> Self;
-}
-impl CANResponseStatus for Period0Status {
-    fn decode(raw_value: u64, timestamp: u32) -> Self {
-        // TODO: need to make sure idle mode actually is correct
-        Self {
-            duty_cycle: ((raw_value & 0xFFFF) as f32) / 32768.0,
-            faults: (raw_value >> 16) & 0xFFFF,
-            sticky_faults: (raw_value >> 32) & 0xFFFF,
-            is_inverted: ((raw_value >> 49) & 1) != 0,
-            idle_mode: match (raw_value >> 57) & 1 {
-                1 => IdleMode::Brake,
-                _ => IdleMode::Coast,
-            },
-            is_follower: ((raw_value >> 58) & 1) != 0,
-            timestamp: std::time::UNIX_EPOCH.add(Duration::from_millis(timestamp as u64)),
-        }
-    }
-}
-impl CANResponseStatus for Period1Status {
-    fn decode(raw_value: u64, timestamp: u32) -> Self {
-        todo!()
-    }
-}
-impl CANResponseStatus for Period2Status {
-    fn decode(raw_value: u64, timestamp: u32) -> Self {
-        todo!()
-    }
-}
-impl CANResponseStatus for Period3Status {
-    fn decode(raw_value: u64, timestamp: u32) -> Self {
-        todo!()
-    }
-}
-impl CANResponseStatus for Period4Status {
-    fn decode(raw_value: u64, timestamp: u32) -> Self {
-        todo!()
-    }
-}
-
 pub struct CANClient {
     device_id: u8,
-    device_type: can_api::CANDeviceType,
-    manufacturer: can_api::CANManufacturer,
     session: hal_can::StreamSession,
 }
 
 impl CANClient {
-    pub fn new(
-        device_id: u8,
-        device_type: can_api::CANDeviceType,
-        manufacturer: can_api::CANManufacturer,
-    ) -> Self {
-        let arb_id = CANClient::create_arb_id_from_info(
-            device_id,
-            device_type,
-            manufacturer,
-            CANCommand::Null,
-        );
+    pub fn new(device_id: u8) -> Self {
         Self {
             device_id,
-            device_type,
-            manufacturer,
             session: hal_can::StreamSession::open(
-                arb_id,
+                SparkCANFrame::heartbeat(device_id as u32), // this needs to be checked
                 0u32 | 0x3F << 16 | 0x3F << 22 | 0x3F << 28,
                 8,
             )
@@ -166,17 +173,8 @@ impl CANClient {
         }
     }
 
-    pub fn create_arb_id(&self, command: CANCommand) -> u32 {
-        CANClient::create_arb_id_from_info(
-            self.device_id,
-            self.device_type,
-            self.manufacturer,
-            command,
-        )
-    }
-
     pub fn send_heartbeat(&self) -> HALResult<()> {
-        let arbitration_id = self.create_arb_id(CANCommand::Heartbeat);
+        let arbitration_id = SparkCANFrame::heartbeat(self.device_id as u32);
 
         hal_can::send_message(arbitration_id, &[0u8; 8], 2000)?;
         Ok(())
@@ -185,7 +183,13 @@ impl CANClient {
     pub fn set_percent(&self, percent: f32) -> HALResult<()> {
         let percent = percent.clamp(-1.0, 1.0);
 
-        let arbitration_id = self.create_arb_id(CANCommand::DutyCycle);
+        let arbitration_id = SparkCANFrame::DutyCycle {
+            setpoint: percent,
+            arb_feedforward: 0,
+            pid_slot: 0,
+            ff_units: FeedforwardUnits::DutyCycle,
+        }
+        .arb_id(self.device_id as u32);
 
         let data = percent.to_le_bytes();
 
@@ -194,7 +198,13 @@ impl CANClient {
     }
 
     pub fn set_voltage(&self, voltage: f32) -> HALResult<()> {
-        let arbitration_id = self.create_arb_id(CANCommand::Voltage);
+        let arbitration_id = SparkCANFrame::Voltage {
+            setpoint: voltage,
+            arb_feedforward: 0,
+            pid_slot: 0,
+            ff_units: FeedforwardUnits::Voltage,
+        }
+        .arb_id(self.device_id as u32);
 
         let data = voltage.to_le_bytes();
 
@@ -208,22 +218,11 @@ impl CANClient {
         0u32 | 0x3F << 16 | 0x3F << 22 | 0x3F << 28
     }
 
-    pub fn create_arb_id_from_info(
-        device_id: u8,
-        device_type: can_api::CANDeviceType,
-        manufacturer: can_api::CANManufacturer,
-        command: CANCommand,
-    ) -> u32 {
-        let api_class = (command as u32) << 4;
-        let api_index = (command as u32) & 0x0F;
-        return ((device_type as u32) << 24)
-            | ((manufacturer as u32) << 16)
-            | api_class << 10
-            | api_index << 6
-            | device_id as u32;
+    pub fn create_arb_id_from_info(device_id: u32, frame: SparkCANFrame) -> u32 {
+        frame.arb_id(device_id) | device_id << 6 // this needs to be double checked
     }
 
-    pub fn read_frames(&self) -> HALResult<Vec<CANResponse>> {
+    pub fn read_frames(&self) -> HALResult<Vec<SparkCANFrame>> {
         let mut messages = [];
         let (_, error) = self.session.read_into(&mut messages);
 
@@ -231,43 +230,27 @@ impl CANClient {
             return Err(HALError::from(error));
         };
 
-        let mut can_responses: Vec<CANResponse> = Vec::with_capacity(messages.len());
+        let mut can_responses: Vec<SparkCANFrame> = Vec::with_capacity(messages.len());
 
         for message in messages {
             let message_id = message.messageID;
-            let mut raw_value = 0u64;
-            for i in 0..message.data.len() {
-                raw_value |= (message.data[i] as u64) << (8 * i);
-            }
 
-            if message_id == self.create_arb_id(CANCommand::Period0) {
-                can_responses.push(CANResponse::Period0(Period0Status::decode(
-                    raw_value,
-                    message.timeStamp,
-                )))
-            } else if message_id == self.create_arb_id(CANCommand::Period1) {
-                can_responses.push(CANResponse::Period1(Period1Status::decode(
-                    raw_value,
-                    message.timeStamp,
-                )))
-            } else if message_id == self.create_arb_id(CANCommand::Period2) {
-                can_responses.push(CANResponse::Period2(Period2Status::decode(
-                    raw_value,
-                    message.timeStamp,
-                )))
-            } else if message_id == self.create_arb_id(CANCommand::Period3) {
-                can_responses.push(CANResponse::Period3(Period3Status::decode(
-                    raw_value,
-                    message.timeStamp,
-                )))
-            } else if message_id == self.create_arb_id(CANCommand::Period4) {
-                can_responses.push(CANResponse::Period4(Period4Status::decode(
-                    raw_value,
-                    message.timeStamp,
-                )))
-            } else {
-                return Err(HALError::DecodeError);
-            }
+            let frame = match message_id {
+                33929216 => SparkCANFrame::Status0 {
+                    applied_output: u16::from_le_bytes(message.data[0..2].try_into().unwrap())
+                        as f32,
+                    voltage: 0f32,
+                    current: 0f32,
+                    motor_temperature: 0u8,
+                    hard_forward_limit_reached: false,
+                    hard_reverse_limit_reached: false,
+                    soft_forward_limit_reached: false,
+                    soft_reverse_limit_reached: false,
+                    is_inverted: false,
+                },
+                _ => continue,
+            };
+            can_responses.push(frame);
         }
         Ok(can_responses)
     }
